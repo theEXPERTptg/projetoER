ac9cb8b35899fdff4e819c3539c8d3e87ef52a47
jdfabs <jdfabs1@gmail.com>
2024-12-12 18:02:54 +0000

    test2

--- plataformatelessaude/app/controllers/settings.js
+++ plataformatelessaude/app/controllers/settings.js
@@ -1,10 +1,20 @@
-
-function startCall() {
-    const peerId = $.peerIdField.value;
+function callPeer() {
+    const peerId = $.peerIdField.value.trim();
+  
     if (peerId) {
+      // Send the Peer ID to the WebView
       $.webRTCView.evalJS(`callPeer('${peerId}')`);
     } else {
-      alert("Enter a Peer ID first.");
+      alert("Please enter a valid Peer ID.");
     }
   }
+ function webViewLoaded(e) {
+    // Ask for the Peer ID from the WebView
+    $.webRTCView.evalJS("peer.id", function(peerId) {
+      alert("Your Peer ID is: " + peerId);
+    });
+  }
   
+  $.webRTCView.addEventListener("load", webViewLoaded);
+
+
