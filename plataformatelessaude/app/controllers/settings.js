7284c6ed65c1d5685931903bdb56435fd29b2de4
jdfabs <jdfabs1@gmail.com>
2024-12-12 17:56:29 +0000

    test2
    video call test
    
    test3
    
    This reverts commit ac9cb8b35899fdff4e819c3539c8d3e87ef52a47.

--- plataformatelessaude/app/controllers/settings.js
+++ plataformatelessaude/app/controllers/settings.js
@@ -1,3 +1,20 @@
+function callPeer() {
+    const peerId = $.peerIdField.value.trim();
+  
+    if (peerId) {
+      // Send the Peer ID to the WebView
+      $.webRTCView.evalJS(`callPeer('${peerId}')`);
+    } else {
+      alert("Please enter a valid Peer ID.");
+    }
+  }
+ function webViewLoaded(e) {
+    // Ask for the Peer ID from the WebView
+    $.webRTCView.evalJS("peer.id", function(peerId) {
+      alert("Your Peer ID is: " + peerId);
+    });
+  }
+  
+  $.webRTCView.addEventListener("load", webViewLoaded);
 
 
-$.mainWindow.open()
