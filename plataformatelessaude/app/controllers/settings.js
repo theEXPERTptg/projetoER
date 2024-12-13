c6946701c4ebf54905bfb184fe92a58aa1a79d25
jdfabs <jdfabs1@gmail.com>
2024-12-13 20:53:14 +0000

    video cleanup

--- plataformatelessaude/app/controllers/settings.js
+++ plataformatelessaude/app/controllers/settings.js
@@ -2,17 +2,16 @@ var webView = $.webView;
     var permissions = ['android.permission.CAMERA', 'android.permission.RECORD_AUDIO'];
     Ti.Android.requestPermissions(permissions, function(e) {
         if (e.success) {
-            // proceed
         } else {
             alert('You need to grant camera and mic permissions to use video calling.');
         }
     });
+
+
 webView.addEventListener('load', function() {
-    // Wait until webView is loaded, then periodically check for currentPeerId
     var checkInterval = setInterval(function() {
         var result = webView.evalJS('currentPeerId');
         if (result && result !== 'null' && result !== '') {
-            // Remove any extra quotes
             result = result.replace(/^"|"$/g, '');
             $.myPeerIdLabel.text = "Your Peer ID: " + result;
             clearInterval(checkInterval);
@@ -23,8 +22,10 @@ webView.addEventListener('load', function() {
 $.callButton.addEventListener('click', function() {
     var friendId = $.peerIdField.value;
     if (!friendId) {
-        alert("Please enter your friend's Peer ID");
+        alert("No ID provided.");
         return;
     }
-    webView.evalJS('startCall("' + friendId + '")');
+    var result = webView.evalJS('startCall("' + friendId + '")');
+    Ti.API.info("Result from evalJS: " + result);
 });
+
