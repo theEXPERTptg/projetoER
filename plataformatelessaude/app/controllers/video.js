3aa645706388d7cf63a579d7a44298a09ce0c639
jdfabs <jdfabs1@gmail.com>
2024-12-15 13:13:11 +0000

    test

--- plataformatelessaude/app/controllers/video.js
+++ plataformatelessaude/app/controllers/video.js
@@ -1,17 +1,18 @@
+var permissions = ['android.permission.CAMERA', 'android.permission.RECORD_AUDIO'];
+Ti.Android.requestPermissions(permissions, function(e) {
+    if (!e.success) {
+        alert('You need to grant camera and mic permissions to use video calling.');
+    }
+});
+
+// Once your layout is ready:
 var webView = $.webView;
-    var permissions = ['android.permission.CAMERA', 'android.permission.RECORD_AUDIO'];
-    Ti.Android.requestPermissions(permissions, function(e) {
-        if (e.success) {
-        } else {
-            alert('You need to grant camera and mic permissions to use video calling.');
-        }
-    });
 
-
+// Require the helper and set the WebView client AFTER webView is created
 webView.addEventListener('load', function() {
     var checkInterval = setInterval(function() {
         var result = webView.evalJS('currentPeerId');
-        Ti.API.info(result);
+        Ti.API.info("currentPeerId: " + result);
         if (result && result !== 'null' && result !== '') {
             result = result.replace(/^"|"$/g, '');
             $.myPeerIdLabel.text = "Your Peer ID: " + result;
@@ -26,6 +27,7 @@ $.callButton.addEventListener('click', function() {
         alert("No ID provided.");
         return;
     }
-    var result = webView.evalJS('startCall("' + friendId + '")');
+    webView.evalJS('startCall("' + friendId + '")');
 });
+
 
