5d44998ae656fd93abc32faec64cd9f7d7e6afe6
GoncasFr <goncasfr@gmail.com>
2024-12-16 10:58:57 +0000

    algumas altreações personalInfo
    A preparar tratamento de mudanças

--- plataformatelessaude/app/controllers/personalInfo.js
+++ plataformatelessaude/app/controllers/personalInfo.js
@@ -17,7 +17,30 @@ function showValues(){
 }
 
 function updateValues(){
+    var name = $.name.value;
+    var gender = $.gender.value;
+    var birthDate = $.birthDate.value;
+    var nifNumber = $.NifNumber.value;
+    var phoneNumber = $.phoneNumber.value;
+    var email = $.email.value;
+    var emergencyName = $.emergencyName.value;
+    var emergencyPhoneNumber = $.emergencyPhoneNumber.value
+    var relationship = $.relationship.value
+
+    if(!$.name.value){
+        goToProfile();
+        $.name.value = loggedInAccount.name;
+        alert(`O campo nome foi deixado em branco. A reverter alteração para o nome anterior: ${loggedInAccount.name}` )
+        return false;
+    } else if($.name.value){
+        loggedInAccount.name = $.name.value;
+        // accounts.accounts.name = $.name.value; ver isto se consigo alterar
+        goToProfile();
+        return true;
+    }
+
 
 }
 
 showValues()
+updateValues()
